3b156c18aa4bce8d0d315a0c9ed74b60f9ea1567
Nemish <sapptest2020@gmail.com>
2025-04-26 09:59:17 +0530

    Fix :- model in data fill and access the value

--- lib/spectrum_data.dart
+++ lib/spectrum_data.dart
@@ -9,11 +9,11 @@ class SpectrumDataChart extends StatefulWidget {
 }
 
 class _SpectrumDataChartState extends State<SpectrumDataChart> {
-  List <SpectrumChartItem> dataPoints = [
-    SpectrumChartItem(freq: 351, reference: 35.23, level: 36.1),
-    SpectrumChartItem(freq: 753, reference: 42.02, level: 41.4),
-    SpectrumChartItem(freq: 1005, reference: 44.83, level: 44.7),
-    SpectrumChartItem(freq: 1701, reference: 47.64, level: 47.8),
+  List<DSPointData> dataPoints = [
+    DSPointData(freq: 351, reference: 35.23, level: 36.1),
+    DSPointData(freq: 753, reference: 42.02, level: 41.4),
+    DSPointData(freq: 1005, reference: 44.83, level: 44.7),
+    DSPointData(freq: 1701, reference: 47.64, level: 47.8),
   ];
 
   @override
@@ -42,10 +42,19 @@ class _SpectrumDataChartState extends State<SpectrumDataChart> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SpectrumBarChart(
-          dataPoints: dataPoints,
+          dataPoints: dataPoints.map((e) => SpectrumChartItem(freq: e.freq.toInt(), level: e.level, reference: e.reference)).toList(),
           dependencies: dependencies,
         ),
       ),
     );
   }
+}
+
+
+class DSPointData {
+  final double freq;
+  final double level;
+  final double reference;
+
+  DSPointData({required this.freq, required this.level, required this.reference});
 }
